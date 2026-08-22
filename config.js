74b0febb81d78dbbe800eba8d1b313bec333769f
Nguyễn Ngọc Hữu <nguyenhung2k4gh@gmail.com>
2026-08-22 16:12:23 +0700

    Update Supabase configuration with URL and anonKey

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = window.APP_CONFIG || {};
 window.APP_CONFIG.supabase = window.APP_CONFIG.supabase || {
-  url: "",
-  anonKey: "",
+  url: "https://oeougzycfplphyaxrknr.supabase.co",
+  anonKey: "sb_publishable_fHh2yBQ5qCSmmKIoghmpuA_tenaz-pb",
 };
